1520ba65845f361e7fc8cb57fe675a3b24c6c7ba
Lidiia A <lida1450@yandex.ru>
2022-08-08 17:25:02 +0300

    new again

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and JS and PizzaCreating
+          Learn React and JS and PizzaCreating!
         </a>
       </header>
     </div>
